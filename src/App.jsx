9a6100787127805a139c5a550ad6ae7ef497d3e5
Faustino <Faustino@LAPTOP-VI7AL9FL>
2021-04-23 14:31:25 -0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { fetchWeather } from './api/fetchWeather'
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+            <h5>Faustino Maggioni Duffy</h5>
+        </div>
+    )
+}
+
+export default App
